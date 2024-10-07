6618f0fa74630dc7c495eba96b1ef0ba71bfd703
Emilian2095 <valentyneemilian@yahoo.com>
2024-10-07 15:39:47 +0200

    Angula day 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { AssetsComponent } from './assets/assets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    HomeComponent,
+    AboutComponent,
+    FooterComponent,
+    AssetsComponent
   ],
   imports: [
     BrowserModule,
